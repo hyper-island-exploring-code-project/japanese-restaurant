ced4c305dffa4a5940b3bf049ebaacbe1a4cf6e7
elen-oz <ozerova.lenka@gmail.com>
2024-05-28 13:02:53 +0200

    ci: fix prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,14 @@
 function App() {
-  return <>
-  <h1 className=" font-thin text-slate-400 text-6xl">Test</h1>
-  </>;
+  return (
+    <>
+      <h1 className="font-thin text-slate-400 text-6xl">Test</h1>
+      <div>
+        <p className="text-red hover:text-blue bg-green-500 font-bold text-2xl">
+          test2
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default App;
